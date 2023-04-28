eaedf3ba7da98f5fbe5060cf5b2286ac48cad7e1
Ant <anthony.hillairet@gmail.com>
2023-04-28 08:07:51 -0700

    :tada: First version with a couple of data points

---
+++ data.js
@@ -0,0 +1,22 @@
+function rangeData() {
+  return [
+    {
+      name: "Exploration and 1st settlements",
+      start: "1497/01/01",
+      end: "1793/01/01"
+    },
+    {
+      name: "Moving towards democracy",
+      start: "1973/01/01",
+      end: "1807/01/01"
+    }
+  ];
+}
+
+// set the moment series data
+function momentData() {
+  return [
+    ["1550/01/01", "\"Canada\" starts appearing on maps"],
+    ["1608/07/03", "Samuel de Champlain builds the 1st fortress in Quebec City"]
+  ];
+}
